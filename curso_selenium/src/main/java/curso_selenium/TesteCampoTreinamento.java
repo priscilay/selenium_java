c26cb7c4afd4dcac240aaeb53d64a60114b14a5e
priscilay <priscila_franca1993@hotmail.com>
2019-07-03 20:10:44 -0300

    mais do curso

--- curso_selenium/src/main/java/curso_selenium/TesteCampoTreinamento.java
+++ curso_selenium/src/main/java/curso_selenium/TesteCampoTreinamento.java
@@ -1,15 +1,21 @@
 package curso_selenium;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 
-	@Test
+
+	/**@Test
+	 * Forçando um erro
 	public void testeTextField(){
 		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,22 +26,142 @@ public class TesteCampoTreinamento {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Priscila");
 		Assert.assertEquals("Prisila", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
-		//driver.quit();
-	}
+		driver.quit();
+	}**/
 	
 	@Test
-	public void testeTextArea(){
+	public void testeTextArea() {
 		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("65file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		System.getProperty("user.dir");
-		
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Priscila");
-		Assert.assertEquals("Prisila", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
-		
-		//driver.quit();
-		
+		Assert.assertEquals("Priscila", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.quit();
+
 	}
 	
+	@Test
+	public void testeRadio() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:1")).isSelected());
+
+		driver.quit();
+
+	}
+	
+	@Test
+	public void testeCombo() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		//combo.selectByIndex(4);
+		//combo.selectByValue("1o grau incompleto");
+		combo.selectByVisibleText("2o grau completo");
+		Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+		
+		driver.quit();
+
+	}
+
+	
+	@Test
+	public void testeComboValores() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		java.util.List<WebElement> options = combo.getOptions();
+		//Assert.assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for (WebElement option : options) {
+			if (option.getText().equals("Mestrado")) {
+				encontrou = true;
+				break;
+			}
+			Assert.assertTrue(encontrou);
+		}
+		driver.quit();
+
+	}
+	
+	@Test
+	public void testeComboValoresMultiplaEscolha() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Karate");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("Futebol");
+
+		List<WebElement> allSelectOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectOptions.size());
+
+		combo.deselectByVisibleText("Karate");
+		allSelectOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectOptions.size());
+	}
+	
+	
+	@Test
+	public void testeBotoes() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement botao = driver.findElement(By.id("buttonSimple")); 
+		botao.click();
+		Assert.assertEquals("Obrigado!", botao.getAttribute("Value"));
+		driver.quit();
+	
+	}
+	
+	
+	@Test
+	public void testeLinks() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement link = driver.findElement(By.linkText("Voltar"));
+		link.click();
+		Assert.assertEquals("Voltou!", driver.findElement(By.id("Resultado")).getText());
+		driver.quit();
+	}
+	
+	
+	@Test
+	public void testeTextoPage() {
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		WebElement texto = driver.findElement(By.tagName("h3"));
+		Assert.assertEquals("Campo de Treinamento", texto.getText());
+		
+		WebElement text2 = driver.findElement(By.className("facilAchar"));
+		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", text2.getText());
+		driver.quit();
+	}
 }
