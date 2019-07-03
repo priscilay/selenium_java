71fafdc12ca0de4e47f268eabedafedf37303797
priscilay <priscila_franca1993@hotmail.com>
2019-07-03 15:56:27 -0300

    Curso selenium do basico ao grid

---
+++ curso_selenium/src/main/java/curso_selenium/TesteCampoTreinamento.java
@@ -0,0 +1,41 @@
+package curso_selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteCampoTreinamento {
+
+	@Test
+	public void testeTextField(){
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Priscila");
+		Assert.assertEquals("Prisila", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		
+		//driver.quit();
+	}
+	
+	@Test
+	public void testeTextArea(){
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		System.getProperty("user.dir");
+		
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Priscila");
+		Assert.assertEquals("Prisila", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		//driver.quit();
+		
+	}
+	
+}
---
+++ curso_selenium/src/main/java/curso_selenium/TesteGoogle.java
@@ -0,0 +1,39 @@
+package curso_selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteGoogle {
+
+	@Test
+	public void teste() {
+		// caso nao estivesse configurado a variavel de ambiente para
+		// chromedriver
+		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//distnacia navegador
+		driver.manage().window().setPosition(new Point(100, 100));
+		
+		//tamanho navegador
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		
+		//maximizar
+		driver.manage().window().maximize();
+		
+		//abrindo a aplicacao
+		driver.get("https://www.google.com.br");
+		
+		//validando titulo
+		String tituloObtido = driver.getTitle();
+		Assert.assertEquals("Google", tituloObtido);
+		
+		//fechando navegador 
+		driver.quit();
+
+	}
+
+}
