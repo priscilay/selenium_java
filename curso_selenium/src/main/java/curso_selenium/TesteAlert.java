7f96263cf54ca1d76db9feed7040704278c7da80
priscilay <priscila_franca1993@hotmail.com>
2019-08-20 13:20:52 -0300

    Curso selenium

--- curso_selenium/src/main/java/curso_selenium/TesteAlert.java
+++ curso_selenium/src/main/java/curso_selenium/TesteAlert.java
@@ -1,5 +1,7 @@
 package curso_selenium;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -11,14 +13,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import junit.framework.Assert;
 
 public class TesteAlert {
-
-	@Test
-	public void clicarAlerta() {
+	
+	private WebDriver driver;
+	private DSL dsl;
+	private CampoTreinamentoPage page;
+	
+	@Before
+	public void init(){
 		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		System.getProperty("user.dir");
+		dsl = new DSL(driver);
+		page = new CampoTreinamentoPage(driver);
+		}
+	
+	@After
+	public void fim() {
+		//driver.quit();
+	}
+	
+
+	@Test
+	public void clicarAlerta() {
 		WebElement botaoAlert = driver.findElement(By.id("alert"));
 		botaoAlert.click();
 		Alert alert =  driver.switchTo().alert();
@@ -28,32 +46,21 @@ public class TesteAlert {
 		WebElement textName = driver.findElement(By.id("elementosForm:nome"));
 		textName.sendKeys(textAlert);
 		//Assert.assertEquals("Alert Simples", )
-		driver.quit();
+
 	}
 	
 	@Test
 	public void clicarAlertaConfirm() {
-		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		System.getProperty("user.dir");
 		WebElement botaoAlertConfirm = driver.findElement(By.id("confirm"));
 		botaoAlertConfirm.click();
 		Alert alertCconfirm = driver.switchTo().alert();
 		alertCconfirm.dismiss();
 		Assert.assertEquals("Negado", alertCconfirm.getText());
 		alertCconfirm.accept();
-		driver.quit();	
 	}
 	
 	@Test
 	public void clicarAlertaPrompt() {
-		System.setProperty("webdriver.chrome.driver", "\\Users\\priscila.franca\\Downloads\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		System.getProperty("user.dir");
 		WebElement botaoAlertPrompt = driver.findElement(By.id("prompt"));
 		botaoAlertPrompt.click();
 		Alert alertPrompt = driver.switchTo().alert();
@@ -63,6 +70,5 @@ public class TesteAlert {
 		Assert.assertEquals("Era 1111?", alertPrompt.getText());
 		alertPrompt.accept();
 		Assert.assertEquals(":D", alertPrompt.getText());
-		driver.quit();	
 	}
 }
